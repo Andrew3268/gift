bccfc3e227d66bfe9b59411effff6a9123e3407e
Your <scarlet3628@likelion.org>
2020-04-30 14:26:29 +0900

    devise production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', :protocal => 'http' }
+  config.action_mailer.default_url_options = { host: '13.125.42.112', :protocal => 'http' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
